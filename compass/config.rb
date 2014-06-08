9ac50179f256e2ec384cffddfe5c2fe0a7b56abd
Maciej Gurban <mgurban@c2server.local>
2014-06-08 21:54:19 +0300

    Updated with SASS mixing.

---
+++ compass/config.rb
@@ -0,0 +1,16 @@
+# Require any additional compass plugins here.
+
+#Folder settings
+relative_assets = true      #because we're not working from the root
+css_dir = ".."              #where the CSS will saved
+sass_dir = "sass"           #where our .scss files are
+images_dir = "../images"    #the folder with your images
+
+# You can select your preferred output style here (can be overridden via the command line):
+output_style = :expanded # dev :expanded, prod :compressed
+
+# To disable debugging comments that display the original location of your selectors. Uncomment:
+line_comments = false
+
+# Obviously
+preferred_syntax = :scss

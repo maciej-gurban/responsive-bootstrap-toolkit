aba0cff6f0dac8e11826b2f6072c76b493fc46f2
Ben Reeves <benwolverine2019@gmail.com>
2016-05-07 17:45:21 -0500

    Added support for Bootstrap 4

--- dist/bootstrap-toolkit.js
+++ dist/bootstrap-toolkit.js
@@ -21,6 +21,14 @@
                 'md': $('<div class="device-md visible-md visible-md-block"></div>'),
                 'lg': $('<div class="device-lg visible-lg visible-lg-block"></div>')
             },
+            // Bootstrap 4
+            bootstrap4: {
+                'xs': $('<div class="hidden-sm-up"></div>'),
+                'sm': $('<div class="hidden-xs-down hidden-md-up"></div>'),
+                'md': $('<div class="hidden-sm-down hidden-lg-up"></div>'),
+                'lg': $('<div class="hidden-md-down hidden-xl-up"></div>'),
+                'xl': $('<div class="hidden-lg-down"></div>')
+            },
             // Foundation 5
             foundation: {
                 'small':  $('<div class="device-xs show-for-small-only"></div>'),
